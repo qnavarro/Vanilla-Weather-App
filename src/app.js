55f4eabcdd51e89b2ce95687705b853584d8d7de
Queenie <queenieb8me@gmail.com>
2023-03-08 13:52:00 -0800

    Set relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -28,16 +28,22 @@ function showTemp(response) {
 	let humidityElement = document.querySelector("#humidity");
 	let windElement = document.querySelector("#wind");
 	let dateElement = document.querySelector("#date");
+	let iconElement = document.querySelector("#icon");
 	temperatureElement.innerHTML = Math.round(response.data.main.temp);
 	cityElement.innerHTML = response.data.name;
 	descriptionElement.innerHTML = response.data.weather[0].description;
 	humidityElement.innerHTML = response.data.main.humidity;
 	windElement.innerHTML = Math.round(response.data.wind.speed);
 	dateElement.innerHTML = formatDate(response.data.dt * 1000);
+	iconElement.setAttribute(
+		"src",
+		`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+	);
+	iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "d55c48b1d5bef2cd3c926fefba853e0e";
-let city = "Los Angeles";
+let city = "Honolulu";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemp);
